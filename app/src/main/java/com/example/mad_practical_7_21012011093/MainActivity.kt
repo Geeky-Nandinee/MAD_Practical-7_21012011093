41bd3859e08b07047af0b7d167eb014b7c23bfb1
student <student@gnu.ac.in>
2023-09-14 08:52:53 +0530

    practical-7

--- app/src/main/java/com/example/mad_practical_7_21012011093/MainActivity.kt
+++ app/src/main/java/com/example/mad_practical_7_21012011093/MainActivity.kt
@@ -1,16 +1,23 @@
 package com.example.mad_practical_7_21012011093
 
+import android.content.Intent
 import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.MediaController
 import android.widget.VideoView
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        val button: FloatingActionButton = findViewById(R.id.floatingActionButton)
+        button.setOnClickListener{
+            Intent(this@MainActivity,YoutubeActivity:: class.java).also{
+                startActivity(it)
+            }
 
         val myVideoview: VideoView = findViewById(R.id.videoview1)
         val mediaController = MediaController( this)
@@ -20,6 +27,9 @@ class MainActivity : AppCompatActivity() {
         myVideoview.setVideoURI(uri)
         myVideoview.requestFocus()
         myVideoview.start()
+
+
+        }
     }
 
 
--- app/src/main/java/com/example/mad_practical_7_21012011093/YoutubeActivity.kt
+++ app/src/main/java/com/example/mad_practical_7_21012011093/YoutubeActivity.kt
@@ -1,5 +1,6 @@
 package com.example.mad_practical_7_21012011093
 
+import android.content.Intent
 import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
@@ -7,11 +8,18 @@ import android.webkit.WebSettings
 import android.webkit.WebView
 import android.widget.MediaController
 import android.widget.VideoView
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class YoutubeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_youtube)
+
+        val button: FloatingActionButton = findViewById(R.id.floatingActionButton)
+        button.setOnClickListener{
+            Intent(this@YoutubeActivity,MainActivity:: class.java).also{
+                startActivity(it)
+            }
 
         val youtubeId = "fjn9B64Znrk"
         val mywebview: WebView = findViewById(R.id.webview)
